a2d2cf911077437431915edcba4f315f441a6c23
itdeer.cn <itdeer@gmail.com>
2018-06-20 11:13:28 +0800

    [itdeerlab-admin-0001] add spring-boot-devtools and test

---
+++ src/main/java/cn/itdeer/modules/admin/user/rest/ApiUserController.java
@@ -0,0 +1,49 @@
+package cn.itdeer.modules.admin.user.rest;
+
+import cn.itdeer.modules.admin.user.entity.User;
+import cn.itdeer.modules.admin.user.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+/**
+ * Description :
+ * PackageName : cn.itdeer.modules.admin.user.rest
+ * ProjectName : itdeerlab-admin
+ * CreatorName : itdeer.cn
+ * CreateTime : 18-6-20/上午9:49
+ */
+
+@Api(value = "用户管理Controller", tags = {"用户操作API"})
+@RestController
+@RequestMapping("/api/admin/user")
+@EnableSwagger2
+public class ApiUserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getUserInfo")
+    @ApiOperation(value = "获取用户信息", tags = {"根据用户ID获取用户的详细信息"}, notes = "ID类型为字符串")
+    public User getUserById(@ApiParam(name = "id", value = "用户ID", required = true) String id){
+        User user = userService.findById(id);
+        return user;
+    }
+
+
+    @GetMapping("/getAllUser")
+    @ApiOperation(value = "获取所有用户信息", tags = {"获取全部用户列表"}, notes = "用户列表")
+    public List<User> getAllUser(){
+        List<User> list = userService.findAll();
+        return list;
+    }
+
+
+}
--- src/main/resources/Instructions.md
+++ src/main/resources/Instructions.md
@@ -6,3 +6,20 @@
 是使用的 [lombok]插件包提供的方法,在编译的使用自动加上Get和Set的方法.
 好处就是不用每一次更改了属性,都要重新去生成Get和Set的方法,节省开发时间.
 ```
+
+
+### 加入Swagger的API文档管理工具
+
+> 使用的一些注解
+
+> API接口查看地址
+
+```
+http://localhost:8080/swagger-ui.html
+```
+
+> 项目访问地址
+
+```
+http://localhost:8080
+```
